d8dbb86b4cf56ba307856e6833108197d8aba537
cullen <kabesh86@gmail.com>
2026-08-21 16:13:37 +0300

    Google analytic

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css"
+import Script from "next/script"
 import { CartProvider } from "@/context/CartContext"
 import Footer from "@/components/Footer"
 
@@ -122,6 +123,21 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="dark">
+        {/* Google Tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LZ6PMKLVG6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-LZ6PMKLVG6');
+          `}
+        </Script>
+
         <CartProvider>
           {children}
           <Footer />
